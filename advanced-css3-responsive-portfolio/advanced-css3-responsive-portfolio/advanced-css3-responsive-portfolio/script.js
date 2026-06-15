6949bfa12b2f64c8f4c988ef583beaa01e09d5b8
MandhraS <smandhralakshmi@gmail.com>
2026-06-15 21:59:08 +0530

    Create script.js

---
+++ advanced-css3-responsive-portfolio/advanced-css3-responsive-portfolio/advanced-css3-responsive-portfolio/script.js
@@ -0,0 +1,18 @@
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("dark-mode");
+
+  if(document.body.classList.contains("dark-mode")){
+
+    toggleBtn.innerHTML = "☀️";
+
+  }
+  else{
+
+    toggleBtn.innerHTML = "🌙";
+
+  }
+
+});
